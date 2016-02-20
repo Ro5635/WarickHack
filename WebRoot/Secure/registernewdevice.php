04c07f65d97787fd2d27d642c27299c6b438fac7
Ryan Fuller <Ryan.Fuller@gradsingames.com>
2016-02-20 18:57:22 +0000

    regester new device form
    /\

--- WebRoot/Secure/registernewdevice.php
+++ WebRoot/Secure/registernewdevice.php
@@ -17,7 +17,28 @@ include('../../Includes/StdImage.php');
 						<span id="PageTitleContainer"><span id="PageTitle"><h1>Register an Arduino</h1></span></span>
 						<span id="ArticleContentContainer">
 
-		
+													<?php //echo '<span id="PageHeadImage"> <img src="' .  cloudFrontCannedPolicyURLSign('https://cdn.ro5635.co.uk/Cone.jpg') . '"></span>'; 
+						//Allow the user to create an account and upload an image to assosiate with the GUI:
+						?>
+						<form action="http://api.arduinowebgui.com/api.php" method='post' id="NameNewArduino">
+							Simple name
+							<br>
+							<input type='text' name='simpleName'/>
+							<input type = 'hidden' name = "TASK" value = 2/>
+							<br>
+							<input type="submit" value="Submit">
+							
+						</form>
+						<form action="http://api.arduinowebgui.com/api.php" method='post' id="HashNewArduino">
+							Hash value
+							<br>
+							<input type='hash' name='HashValue'/>
+							<br>
+							<input type="submit" value="Submit">
+							
+
+						</form>
+
 
 					</span>
 		</article>
