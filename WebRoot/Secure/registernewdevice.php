e3ddf6b4c5719b164d705ec42b77e28313d883b6
Ro5635 <ro5635@gmail.com>
2016-02-20 18:48:14 +0000

    page created reg arduino

---
+++ WebRoot/Secure/registernewdevice.php
@@ -0,0 +1,40 @@
+<?php
+$pageTitle = 'Register an Arduino';
+$pageDescription = 'Register your arduino';
+
+require_once('../../Includes/CheckLogIn.php');
+require_once('../../Includes/StdAdminHead.php');
+
+echo '<span Class="Page"><section>';
+
+include('../../Includes/StdImage.php');
+
+
+
+?>
+
+		<article id="MainPageContent">
+						<span id="PageTitleContainer"><span id="PageTitle"><h1>Register an Arduino</h1></span></span>
+						<span id="ArticleContentContainer">
+
+		
+
+					</span>
+		</article>
+
+
+<?php
+echo '	</section></span>';
+include('../../Includes/StdFooter.php');
+?>
+
+
+	
+	
+		
+
+		
+
+
+
+	
